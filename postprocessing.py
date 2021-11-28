1bb61cbf5e82147efc0a1ca1a7afd8800eaf31aa
Yana241996 <83159431+Yana241996@users.noreply.github.com>
2021-11-28 10:58:11 +0300

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -4,10 +4,10 @@ from feedgen.feed import FeedGenerator
 from datetime import datetime, timezone
 
 # Normal GitHub Pages URL
-# ghPagesURL = "https://conoro.github.io/tiktok-rss-flat/"
+# ghPagesURL = "https://yana241996.github.io/tiktok-rss-flat/"
 
 # Custom Domain
-ghPagesURL = "https://tiktokrss.conoroneill.com/"
+ghPagesURL = "https://yana241996.github.io/tiktok-rss-flat/"
 
 api = TikTokApi.get_instance()
 
